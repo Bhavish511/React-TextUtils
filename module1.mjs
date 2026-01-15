987e69073c9438786febda9ac72c1b44e0f6ddbc
Bhavish511 <Bhavishsindhi3@gmail.com>
2026-01-15 14:23:51 +0500

    Props, propTypes and Default props

---
+++ module1.mjs
@@ -0,0 +1,7 @@
+const a = "Bhavi";
+const b = "Kothari";
+const c = "Hello World!";
+
+export default a;
+export { b };
+export { c };
---
+++ module2.mjs
@@ -0,0 +1,5 @@
+import dz, { b, c } from "./module1.mjs";
+
+console.log(dz);
+console.log(b);
+console.log(c);
