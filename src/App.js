f00d5eecbc9a6e0f87c1cc042d294bdf32a3c8b1
Bhavish511 <Bhavishsindhi3@gmail.com>
2026-01-17 21:26:49 +0500

    Added the React-Router-dom implementation and also document the React-router-dom

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  // Link
+} from "react-router-dom";
 function App() {
   const [mode, setMode] = useState('light');
   const [alert, setAlert] = useState(null);
@@ -42,12 +49,17 @@ function App() {
   } 
   return (
     <>
-      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
-      <Alert alert={alert}/>
-      <div className="container" my-3>
-        <TextForm setAlertMessage={setAlertMessage} heading="Enter the text to analyze" mode={mode}/>
-      </div>
-      <About/>
+      <Router>
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
+        <Alert alert={alert}/>
+        <div className="container" my-3>
+          {/* <TextForm setAlertMessage={setAlertMessage} heading="Enter the text to analyze" mode={mode}/> */}
+          <Routes>
+            <Route path="/about" element={<About />} />
+            <Route path="/" element={<TextForm setAlertMessage={setAlertMessage} heading="Enter the text to analyze" mode={mode}/>} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
